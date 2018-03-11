0c6652f6ad205956a219f8638f4e768e78a0741d
Administrator <Administrator@YLMF-1704081533>
2018-03-11 23:56:59 +0800

    启动说明

--- src/main/java/com/example/demo/ConfigApplication.java
+++ src/main/java/com/example/demo/ConfigApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 /**
  * 发现服务
+ * 启动顺序：发现服务->配置中心->数据服务->web服务->监控服务
  * @author mhs123
  *
  */
