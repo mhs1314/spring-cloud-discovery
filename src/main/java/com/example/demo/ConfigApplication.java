bdb6451d7c387d321c088b615422c2b30fd6727b
Administrator <Administrator@YLMF-1704081533>
2018-03-11 23:47:16 +0800

    注释添加

--- src/main/java/com/example/demo/ConfigApplication.java
+++ src/main/java/com/example/demo/ConfigApplication.java
@@ -3,6 +3,11 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+/**
+ * 发现服务
+ * @author mhs123
+ *
+ */
 @EnableEurekaServer
 @SpringBootApplication
 public class ConfigApplication {
